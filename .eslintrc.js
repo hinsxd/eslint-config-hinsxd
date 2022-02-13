325441d82edd2b0555e40b47a0bf5bc73e153d49
Alvin Li <hins.xd@hotmail.com>
2022-02-14 02:34:05 +0800

    feat: revert 0.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -88,7 +88,6 @@ module.exports = {
       extends: ["next", "next/core-web-vitals"],
       rules: {
         "react/display-name": "off",
-        "react/button-has-type": "error",
       },
     },
   ],
